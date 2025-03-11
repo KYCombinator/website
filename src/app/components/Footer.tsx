dd42460e2830c65b3f1183144efc0002c0686602
wdpowell03 <wdpowell03@gmail.com>
2025-03-11 15:15:30 -0400

    velocity fix and same purple throughout web ap

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -46,9 +46,6 @@ export default function Footer() {
                 </Link>
               </li>
               <li>
-                <Link href="/velocity" className="text-gray-400 hover:text-white transition">
-                  Velocity Pitch
-                </Link>
               </li>
               <li>
                 <Link href="https://www.hackkentucky.com" className="text-gray-400 hover:text-white transition">

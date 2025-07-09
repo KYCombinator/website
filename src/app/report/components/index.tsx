36735c4e678d6743d3a36d4e56dd7f46e104bb80
Dan Ross-Li <me@danrossli.com>
2025-07-09 09:57:24 -0400

    add slug

---
+++ src/app/report/components/index.tsx
@@ -0,0 +1,11 @@
+import NYTimesAIGeography from "./NYTimesAIGeography";
+import KYCombinatorDemographic from "./KYCombinatorDemographic";
+import NewYorker from "./NewYorker";
+
+export const reportComponents = {
+  "nytimes-ai-geography": NYTimesAIGeography,
+  "kycombinator-demographic": KYCombinatorDemographic,
+  newyorker: NewYorker,
+} as const;
+
+export type ReportComponentKey = keyof typeof reportComponents;

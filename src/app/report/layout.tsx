3b9f505be160e520314716c1e5fd48b1098367c6
Dan Ross-Li <me@danrossli.com>
2025-07-09 09:51:40 -0400

    add report route

---
+++ src/app/report/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Reports | KYC",
+  description:
+    "Acting in the present defines the future. Defining our future contextualizes our past. Explore data and insights from KYCombinator",
+  openGraph: {
+    images: ["/futurelou.png"],
+  },
+};
+
+export default function ReportsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen flex flex-col container mx-auto">
+      {children}
+    </div>
+  );
+}

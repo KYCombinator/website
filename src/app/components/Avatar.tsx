e7baf1220c97a995bced305b5272bd5efbdf5e3d
Dan Ross-Li <me@danrossli.com>
2025-07-20 14:16:47 -0400

    add hzzh types

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -3,24 +3,21 @@
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 import Link from "next/link";
+import { AuthUser } from "@hzzhsoftware/types-auth";
 
 const Avatar = async () => {
   const cookieStore = await cookies();
   const token = cookieStore.get(`hzzh.${process.env.NEXT_PUBLIC_APP_ID}.token`)?.value;
 
-  let user: {
-    email: string | null;
-    name: string | null;
-    picture: string | null;
-  } | null = null;
+  let user: AuthUser | null = null;
 
   if (token) {
     const decoded = jwt.decode(token) as { email?: string; name?: string; picture?: string } | null;
     if (decoded) {
       user = {
-        email: decoded.email || null,
-        name: decoded.name || null,
-        picture: decoded.picture || null,
+        email: decoded.email || "",
+        name: decoded.name || "",
+        picture: decoded.picture || "",
       };
     }
   }

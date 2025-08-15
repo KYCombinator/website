e0fe4ed18eb72997601052ee80f23ea6993a1232
Dan Ross-Li <me@danrossli.com>
2025-08-14 22:02:51 -0400

    update metatags

--- src/app/events/velocity/layout.tsx
+++ src/app/events/velocity/layout.tsx
@@ -2,20 +2,20 @@ import type React from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Velocity Pitch Competition | KY Combinator",
+  title: "Velocity Gauntlet | KY Combinator",
   description:
-    "Join the Velocity Pitch Competition - a 12-week startup competition in Louisville showcasing high-growth potential startups. Register to attend or apply to pitch your startup.",
+    "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
   openGraph: {
-    title: "Velocity Pitch Competition | KY Combinator",
+    title: "Velocity Gauntlet | KY Combinator",
     description:
-      "Join the Velocity Pitch Competition - a 12-week startup competition in Louisville showcasing high-growth potential startups. Register to attend or apply to pitch your startup.",
+      "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
     images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity.png`],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Velocity Pitch Competition | KY Combinator",
+    title: "Velocity Gauntlet | KY Combinator",
     description:
-      "Join the Velocity Pitch Competition - a 12-week startup competition in Louisville showcasing high-growth potential startups. Register to attend or apply to pitch your startup.",
+      "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
     images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity.png`],
   },
 };

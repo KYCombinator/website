365457fc7cca88f097a5538adb514883148e361d
danrossli-ionwater <danrossli@ionwater.io>
2025-11-16 13:36:11 -0500

    change redirect

--- src/app/(events)/louies/page.tsx
+++ src/app/(events)/louies/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 
 // You can update this to change the current year's redirect
-const CURRENT_YEAR = "2024";
+const CURRENT_YEAR = "2025";
 
 export default function LouiesRedirect() {
   redirect(`/louies/${CURRENT_YEAR}`);

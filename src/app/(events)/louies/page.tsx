5b1879c03c7d4024253711dd50cb5447e643253d
danrossli-ionwater <danrossli@ionwater.io>
2025-11-16 13:46:38 -0500

    remove pages

--- src/app/(events)/louies/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-// You can update this to change the current year's redirect
-const CURRENT_YEAR = "2025";
-
-export default function LouiesRedirect() {
-  redirect(`/louies/${CURRENT_YEAR}`);
-}

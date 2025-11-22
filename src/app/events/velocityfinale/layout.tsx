cece4ac8e84f6424d49953c5f70947ba9cc4fd1f
danrossli-ionwater <danrossli@ionwater.io>
2025-11-21 22:06:21 -0500

    update meta

--- src/app/events/velocityfinale/layout.tsx
+++ src/app/events/velocityfinale/layout.tsx
@@ -2,20 +2,20 @@ import type React from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Velocity Gauntlet | KY Combinator",
+  title: "Velocity Demo Day | KYX",
   description:
-    "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
+    "Watch 5 startups present their 12-week journey at Velocity Demo Day. Part of the Velocity / The LOUIES - KYX 2025 Celebration on December 4th in Louisville, KY.",
   openGraph: {
-    title: "Velocity Gauntlet | KY Combinator",
+    title: "Velocity Demo Day | KYX",
     description:
-      "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
+      "Watch 5 startups present their 12-week journey at Velocity Demo Day. Part of the Velocity / The LOUIES - KYX 2025 Celebration on December 4th in Louisville, KY.",
     images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity/VelocityMeta.jpg`],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Velocity Gauntlet | KY Combinator",
+    title: "Velocity Demo Day | KYX",
     description:
-      "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
+      "Watch 5 startups present their 12-week journey at Velocity Demo Day. Part of the Velocity / The LOUIES - KYX 2025 Celebration on December 4th in Louisville, KY.",
     images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity/VelocityMeta.jpg`],
   },
 };

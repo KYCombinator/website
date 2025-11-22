94c72fc1e12345e4e257c111753f0cbadc3cee4e
danrossli-ionwater <danrossli@ionwater.io>
2025-11-21 22:00:49 -0500

    change velocity meta

--- src/app/events/velocityfinale/layout.tsx
+++ src/app/events/velocityfinale/layout.tsx
@@ -9,14 +9,14 @@ export const metadata: Metadata = {
     title: "Velocity Gauntlet | KY Combinator",
     description:
       "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
-    images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity.png`],
+    images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity/VelocityMeta.jpg`],
   },
   twitter: {
     card: "summary_large_image",
     title: "Velocity Gauntlet | KY Combinator",
     description:
       "Join the Velocity Gauntlet - a 12-week startup incubation for the best in Louisville. Register to attend or apply to pitch your startup.",
-    images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity.png`],
+    images: [`${process.env.NEXT_PUBLIC_CDN_URL}/velocity/VelocityMeta.jpg`],
   },
 };
 
